2efdf75c594b0a621f3bd23c86510cf9ef44a34b
Xiaoyi Liu <lxy878@gmail.com>
2020-03-12 12:34:51 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,17 @@
 class Customer
-  
+  attr_accessor :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    new_meal = Meal.new()
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
+  @@all = []
 
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
