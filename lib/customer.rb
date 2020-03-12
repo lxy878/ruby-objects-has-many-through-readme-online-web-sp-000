47853897fb7881d85c4cabcdda3de1530b3e6a09
Xiaoyi Liu <lxy878@gmail.com>
2020-03-12 13:30:38 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,25 @@
 class Customer
-  
+  attr_accessor :name, :age, :meals
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  def meals
+    Meal.all.find_all {|meal| meal.customer == self}
+  end
+
+  def waiters
+    orders = meals
+    orders.collect {|order| order.waiter}
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
 class Waiter
+  @@all = []
+  attr_accessor :name, :experience
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.find_all {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    orders = meals
+    order = orders.max do |max, order|
+      max.tip <=> order.tip
+    end
+    order.customer
+  end
 end
